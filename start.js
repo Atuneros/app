e909c7bba18931ee21a30ed59ddd4369f0688db3
Atuneros <jovapal.spam92@gmail.com>
2021-02-11 20:21:25 +0100

    implementacion motor de plantillas pug

--- start.js
+++ start.js
@@ -2,18 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set("view engine", "pug")
+app.set("views", process.cwd() + "/views")
+
 const path = require('path')
-app.use(express.static('public'));
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 var data = require('./datos_bolsa.json');
 
-app.get("/loadData",function(request, response){
-  response.header('Access-Control-Allow-Origin', "*");
-  response.header('Access-Control-Allow-Methods', 'GET');
-  response.header("Access-Control-Allow-Headers", "accept, content-type");
-  response.header("Access-Control-Max-Age", "1728000");
-  response.json(data)
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
+app.get("/loadData",(req, res) => {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header("Access-Control-Allow-Headers", "accept, content-type");
+  res.header("Access-Control-Max-Age", "1728000");
+  res.json(data)
 })
 
 app.listen(port, () => {
