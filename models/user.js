27eb9aa00a774fafc4c864e647d5254dfe335184
Atuneros <jovapal.spam92@gmail.com>
2021-05-30 15:15:40 +0200

    arreglando

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const dbURI = "mongodb://editorUser:ClaveMongo666@15.188.13.160:27017/creds"
+const conn = mongoose.createConnection(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cartera:{
+        type: Number,
+        required: false
+    },
+    acciones:{
+        type: Map,
+        of: Number,
+        required: false
+    }
+}, {timestamps: true})
+
+//DEFINO LA COLECCION (LA PRIMERA EN MAYUSCULA) Y EL ESQUEMA QUE VOY A UTILIZAR PARA LAS QUERYS
+const User = conn.model("User", userSchema)
+module.exports = User
